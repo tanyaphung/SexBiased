d6e0618443e1658c3fda7b1e79244bbf31fb4dfd
tnphung <tnphung@ucla.edu>
2018-02-21 15:05:19 -0800

    Add script

---
+++ filter_nucleotide_sites/1_rm_sites_under_direct_sel/exclude_ensemble.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd Sex_Biased_Demography/download/ensemble
+
+for i in {1..38} X
+do
+# Find intervals that do not overlap with the gene by using bedtools complement
+
+. /u/local/Modules/default/init/modules.sh
+module load bedtools
+
+bedtools complement -i chr${i}/chr${i}_ensemble_clean_sort.bed -g /u/project/klohmuel/tanya_data/Canines_Project_Data/chrLength/chr${i}.g > chr${i}/chr${i}_outside_ensemble.bed
+done
