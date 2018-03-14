31d532369692ff8435c1e7fb5bed74246313b092
tnphung <tnphung@ucla.edu>
2018-03-14 08:52:19 -0700

    Fix description of input 2

--- compute_q_pi/1_compute_uncorrected_pi/compute_diversity.py
+++ compute_q_pi/1_compute_uncorrected_pi/compute_diversity.py
@@ -5,7 +5,7 @@ def computePi(AF, numAlleles):
 	""" 
 	This script compute pi using allele frequency. 
 	Input 1 is a list where each item is the allele frequency of the variant.
-	Input 2 is the number of individuals.
+	Input 2 is the number of alleles (which is equal to the number of individuals * 2)
 	Return 0 if the list is empty. The reason why 0 should be returned here is that if the list is empty, it means that there are no variants in that region. If there is no variant, then pi should be 0.
 	NOTE that this is not per site. In order to get per site pi, need to divide by the callable sites.
 	"""
